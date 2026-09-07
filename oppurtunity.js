a4310e2a99041d3e9ccd5cc2eace06b75806327d
PasupulaVarun <pasupulavarun9347@gmail.com>
2026-09-07 11:28:26 +0530

    Update oppurtunity.js

--- oppurtunity.js
+++ oppurtunity.js
@@ -4,7 +4,7 @@
 const API_URL =
     (window.location.hostname === "localhost" ||
      window.location.hostname === "127.0.0.1")
-        ? "http://127.0.0.1:8000recommend"
+        ? "http://127.0.0.1:8000/recommend"
         : "recommend";
 
         // ========================================================
